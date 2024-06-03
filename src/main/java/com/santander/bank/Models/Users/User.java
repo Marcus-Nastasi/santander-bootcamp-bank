b9fdbe3d4d1e0a534f56d80a4b1a392b3083c52f
rolemberg <vinnie.nsts@gmail.com>
2024-06-03 11:06:18 -0300

    created User Entity;

---
+++ src/main/java/com/santander/bank/Models/Users/User.java
@@ -0,0 +1,56 @@
+package com.santander.bank.Models.Users;
+
+import jakarta.persistence.*;
+
+import java.math.BigInteger;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    @Column
+    private String name;
+    @Column
+    private String account_id;
+    @Column
+    private List<BigInteger> card_id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAccount_id() {
+        return account_id;
+    }
+
+    public void setAccount_id(String account_id) {
+        this.account_id = account_id;
+    }
+
+    public List<BigInteger> getCard_id() {
+        return card_id;
+    }
+
+    public void setCard_id(List<BigInteger> card_id) {
+        this.card_id = card_id;
+    }
+}
+
+
+
