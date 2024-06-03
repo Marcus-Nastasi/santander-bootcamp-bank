e0312edfd56309f41c533145d85c7f177b349a1d
rolemberg <vinnie.nsts@gmail.com>
2024-06-03 11:09:17 -0300

    created Account Entity;

---
+++ src/main/java/com/santander/bank/Models/Accounts/Account.java
@@ -0,0 +1,26 @@
+package com.santander.bank.Models.Accounts;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "accounts")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    @Column
+    private String number;
+    @Column
+    private String agency;
+    @Column(precision = 13, scale = 2)
+    private BigDecimal balance;
+    @Column(precision = 13, scale = 2)
+    private BigDecimal limits;
+}
+
+
+
+
