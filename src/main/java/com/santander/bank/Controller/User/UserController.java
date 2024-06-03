512eda02ec3e07660f899e85f0b746bc082a2273
rolemberg <vinnie.nsts@gmail.com>
2024-06-03 12:55:59 -0300

    created UserController;

---
+++ src/main/java/com/santander/bank/Controller/User/UserController.java
@@ -0,0 +1,30 @@
+package com.santander.bank.Controller.User;
+
+import com.santander.bank.Models.Users.User;
+import com.santander.bank.Repository.User.UserRepo;
+import com.santander.bank.Services.User.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api/user")
+public class UserController {
+
+    @Autowired
+    private UserRepo userRepo;
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAll() {
+        return ResponseEntity.ok(userRepo.findAll());
+    }
+}
+
+
+
