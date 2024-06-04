d3575dfbd771e8ae38d481bf7320f87675affc1c
rolemberg <vinnie.nsts@gmail.com>
2024-06-03 23:00:57 -0300

    implemented JWT filter ;

---
+++ src/main/java/com/santander/bank/Infra/Security/JwtFilter.java
@@ -0,0 +1,51 @@
+package com.santander.bank.Infra.Security;
+
+import com.santander.bank.Repository.User.UserRepo;
+import com.santander.bank.Services.Token.TokenService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+    @Autowired
+    private UserRepo userRepo;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = this.getToken(request);
+
+        if (token != null) {
+            String cpf = tokenService.validate(token);
+            UserDetails u = userRepo.findByCpf(cpf);
+
+            var auth = new UsernamePasswordAuthenticationToken(u, null, u.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String getToken(HttpServletRequest request) {
+        String headers = request.getHeader("Authorization");
+        if (headers == null) return null;
+        return headers.replace("Bearer ", "");
+    }
+}
+
+
+
+
+
--- src/main/java/com/santander/bank/Infra/Security/SecurityConfig.java
+++ src/main/java/com/santander/bank/Infra/Security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.santander.bank.Infra.Security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -8,10 +9,14 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private JwtFilter jwtFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -23,6 +28,7 @@ public class SecurityConfig {
                             .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                             .anyRequest().authenticated()
                     )
+                    .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                     .build()
         );
     }
