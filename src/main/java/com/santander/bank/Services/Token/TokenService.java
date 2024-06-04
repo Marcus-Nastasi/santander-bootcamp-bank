4a2305dd2bf32d52ad4bca6ad0e76d81da9cef95
rolemberg <vinnie.nsts@gmail.com>
2024-06-03 22:01:27 -0300

    created TokenService;

--- src/main/java/com/santander/bank/Services/Token/TokenService.java
+++ src/main/java/com/santander/bank/Services/Token/TokenService.java
@@ -1,11 +1,18 @@
 package com.santander.bank.Services.Token;
 
+import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.santander.bank.Models.Users.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 @Service
 public class TokenService {
@@ -18,9 +25,23 @@ public class TokenService {
     public String generate(User user) {
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
-
-
+            return JWT.create().withIssuer("bank-api").withSubject(user.getCpf()).withExpiresAt(exp()).sign(algorithm);
+        } catch (JWTCreationException e) {
+            throw new RuntimeException(e.getMessage());
         }
+    }
+
+    public String validate(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm).withIssuer("bank-api").build().verify(token).getSubject();
+        } catch (JWTVerificationException e) {
+            return "";
+        }
+    }
+
+    private Instant exp() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
     }
 }
 
