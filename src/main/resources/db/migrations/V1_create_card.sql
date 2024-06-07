78d86b39970ebb03e68eacb92c0b8f02ffe178fe
rolemberg <vinnie.nsts@gmail.com>
2024-06-06 23:18:02 -0300

    refactored Card migrations;

--- src/main/resources/db/migrations/V1_create_card.sql
+++ src/main/resources/db/migrations/V1_create_card.sql
@@ -1,7 +1,8 @@
 CREATE TABLE cards(
     id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     number VARCHAR(255) NOT NULL,
-    limits DECIMAL(13, 2)
+    limits DECIMAL(13, 2),
+    limit_spent DECIMAL(13, 2)
 );
 
 
